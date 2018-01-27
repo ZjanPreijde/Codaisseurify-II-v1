49f2823f2fae969572da5867ca95b66cc60067c8
ZjanPreijde <j.preijde2@upcmail.nl>
2018-01-27 15:40:53 +0100

    Button Add adds to table with click event working on delete button

--- app/assets/javascripts/artists.js
+++ app/assets/javascripts/artists.js
@@ -6,7 +6,7 @@
 //  new line in HTML after > shows return sign,
 //  keep text jammed between >< to let innerHTML show pure text
 
-$(function () {
+$(function() {
   // short hand syntax : $(function() {});
   // short for $.( document ).ready(function () {});
   // console.log("Artists.js document ready");
@@ -14,39 +14,27 @@ $(function () {
   //   id="song-delete-all"
   // Delete song :
   //   id="song-delete-<%= song.id %>"
-  //   artist_id="<%= artist.id %>"
-  //   song_id="<%= song.id %>"
   //   class="song-delete
   //
-  consoleLogSongs();
   // bind events
-  $("#song-delete-all").bind( 'click', deleteAllSongsClick );
-  $(".song-delete").bind( 'click', deleteSongClick );
-  $("#add-song").bind( 'click', addSongClick );
+  console.log("Binding events ...");
+  $("#song-delete-all").bind('click', deleteAllSongsClick);
+  $(".song-delete").bind('click', deleteSongClick);
+  $("#add-song").bind('click', addSongClick);
 });
 
-function deleteAllSongsClick( event ) {
+function deleteAllSongsClick(event) {
   // Delete a song
   // Ask confirmation
   console.log("Delete all songs clicked");
-  $(".song-delete").each( function( index, element ) {
-    deleteSong( false );
+  $(".song-delete").each(function(index, element) {
+    deleteSong(false);
   });
 };
-function deleteSongClick( event ) {
+
+function deleteSongClick(event) {
   console.log("Delete song clicked");
   deleteSong();
-};
-
-function addSongClick( event ) {
-  // Add a song
-  // - check for empty
-  // - add row to table
-  console.log("Add song clicked");
-  let title = $("#new-title").val();
-  console.log("- Title :", title);
-
-  $("#new-title").val(null);
 };
 
 function deleteSong(confirm = true) {
@@ -59,14 +47,63 @@ function deleteSong(confirm = true) {
   }
 };
 
+function addSongClick(event) {
+  // Add a song
+  // - check for empty
+  // - add row to table
+  console.log("Add song clicked");
+  let title = $("#new-title").val();
+  console.log("- Title :", title);
+  if (title == "") {
+    console.log("Title is empty")
+  } else {
+    addSongToTable(title);
+    // let song = { :title: title, :id 0, :artist_id 0}
+    $("#new-title").val(null);
+  }
+};
+
+function addSongToTable(title) {
+  console.log("Adding", title, "to <table>");
+
+  let tableRow = $("<tr></tr>");
+
+  let rowDetail1 = $("<td></td>");
+  rowDetail1.attr("id", "song-xx");
+  rowDetail1.addClass("song");
+  rowDetail1.html(title);
+
+  let rowDetail2 = $("<td></td>");
+  rowDetail2.html("#0-0");
+
+  let rowDetail3 = $("<td></td>");
+  rowDetail3.addClass("text-center");
+
+  let rowDet3Anchor = $("<a></a>");
+  rowDet3Anchor.attr("href", "#");
+  rowDet3Anchor.attr("id", "song-delete-0");
+  rowDet3Anchor.addClass("song-delete");
+  rowDet3Anchor.addClass("btn");
+  rowDet3Anchor.addClass("btn-warning");
+  rowDet3Anchor.html("Delete");
+  rowDet3Anchor.bind("click", deleteSongClick);
+
+  rowDetail3.append( rowDet3Anchor );
+
+  tableRow.append( rowDetail1 );
+  tableRow.append( rowDetail2 );
+  tableRow.append( rowDetail3 );
+
+  $("#song-table").append( tableRow );
+};
 
 // old stuff
-function consoleLogSongs () {
+function consoleLogSongs() {
   // <td id="song-<%= song.id %>" class="song">
   let songs = $(".song");
   let songCount = songs.length;
   console.log(songCount.toString() + ' songs found')
-  songs.each( function( index, element ) {
+  songs.each(function(index, element) {
     console.log(element.innerHTML)
   });
 };
