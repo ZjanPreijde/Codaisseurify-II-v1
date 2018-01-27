0bf771e152f99ab887d964917a6f66e1124f2cd7
ZjanPreijde <j.preijde2@upcmail.nl>
2018-01-27 12:06:35 +0100

    Added app/assets/javascript/artists.js with console.logging .ready()

---
+++ app/assets/javascripts/artists.js
@@ -0,0 +1,6 @@
+// apps/assets/javascript/artists.js
+// loaded every artist index/show?
+$( document ).ready(function () {
+  // short hand syntax : $(function() {});
+  console.log("Artists.js document ready");
+});
