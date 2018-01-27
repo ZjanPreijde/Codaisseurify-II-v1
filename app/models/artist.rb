f6185f4b3b36586e3e1fb642152a1bd468f42340
ZjanPreijde <j.preijde2@upcmail.nl>
2018-01-27 10:50:54 +0100

    Updated (shared/artist) views for Codaisseur-II

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,6 +1,6 @@
 class Artist < ApplicationRecord
   has_many :songs, dependent: :destroy
-  mount_uploader :image, ImageUploader
+  # mount_uploader :image, ImageUploader
 
   validates :name, presence: true
   # validates :image, length: {maximum: 255}
