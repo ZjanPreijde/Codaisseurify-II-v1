d27a8d9d01e14449b6fc3af3b70e03546331260a
ZjanPreijde <j.preijde2@upcmail.nl>
2018-01-28 12:09:53 +0100

    Added and tested Integration Test : deleting a song

--- spec/features/manage_tasks_spec.rb
+++ spec/features/manage_tasks_spec.rb
@@ -4,39 +4,31 @@ require 'rails_helper'
 feature 'Manage tasks', js: true do
   before :each do
     Artist.create(name: 'Manage Task Artist')
+    Artist.first.songs.create(title: 'Manage task, song 1')
+    Artist.first.songs.create(title: 'Manage task, song 2')
   end
 
   scenario 'add a new song' do
-    new_song_title = 'Manage task, add new song test'
-
     visit artist_path(1)
 
+    new_song_title = 'Manage task, add new song test'
     fill_in 'new-title', with: new_song_title
-
     page.click_on('add-song')
 
     expect(page).to have_content(new_song_title)
   end
 
-  # scenario 'delete a song' do
-  #
-  #   visit artist_path(1)
-  #
-  #   songs = page.execute_script("$('.song-delete')")
-  #   song_count = songs.length
-  #   # JS or Ruby?
-  #   # if (song_count > 0) {
-  #     delete_id = "#" + songs[0].id
-  #
-  #     accept_confirm do
-  #       page.click_on(delete_id)
-  #     end
-  #     # alternative : dismiss_confirm do
-  #
-  #     # expect( page ).to not_have_content("#" + delete_id)
-  #     new_count = page.execute_script("$('.song_delete')").length
-  #     expect( new_count ).to eq (song_count - 1)
-  #   # }
-  # end
+  scenario 'delete a song' do
+    visit artist_path(1)
+
+    accept_confirm do
+      page.click_on('song-delete-1')
+    end
+    # alternative : dismiss_confirm do
+
+    sleep(2) # Yes, yes, I should not use this
+
+    expect( page ).to have_no_content('Manage task, song 1')
+  end
 
 end
